b340f42a1acfbce819b40017939bee57c8e15594
ElNao28 <naopubg28@gmail.com>
2024-09-28 20:16:50 -0600

    directorio de carpetas creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,9 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  { path: 'Login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'Login', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'Login', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
 ];
 
 @NgModule({
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './pages/home.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomePage,
-  }
-];
+const routes: Routes = [{}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class LoginRoutingModule { }
