925dd29962d892b64ea3b9d6564fdfb541263c20
elNao28 <naopubg28@gmail.com>
2024-10-08 14:58:51 -0600

    login: create recuperar-pasword component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,12 +5,14 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { IonicModule } from '@ionic/angular';
 import { CrearCuentaPage } from './pages/crear-cuenta/crear-cuenta.page';
+import { RecuperarPasswordComponent } from './pages/recuperar-password/recuperar-password.component';
 
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    CrearCuentaPage
+    CrearCuentaPage,
+    RecuperarPasswordComponent,
   ],
   imports: [
     CommonModule,
