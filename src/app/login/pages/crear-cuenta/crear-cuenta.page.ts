22ec20b86988fa8a39aec26a4e7155b4c590e4e2
Mabel1910 <mabelbaam19@gmail.com>
2024-10-11 15:43:50 -0600

    login: crear-cuenta->Creacion del formulario

--- src/app/login/pages/crear-cuenta/crear-cuenta.page.ts
+++ src/app/login/pages/crear-cuenta/crear-cuenta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-cuenta',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCuentaPage{
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
+  public formCreate:FormGroup = this.fb.group({
+    name:['',[Validators.required]],
+    lastname:['',[Validators.required]],
+    motherLastname:['',[Validators.required]],
+    gender:['',[Validators.required]],
+    birthdate:['',[Validators.required]],
+    question:['',[Validators.required]],
+    email:['',[Validators.required]],
+    answer:['',[Validators.required]],
+    cellphone:['',[Validators.required]],
+    password:['',[Validators.required]],
+    password2:['',[Validators.required]]
+    
+  })
   public isNext:boolean = false;
 }
