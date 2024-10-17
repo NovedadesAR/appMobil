be223c4216ccc6dc411a28e2a4371d8fef4bf60e
ElNao <naopubg28@gmail.com>
2024-10-17 10:05:13 -0600

    merge login module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_api: 'http://localhost:3000/'
+  url_api: 'https://0a2a-187-249-108-36.ngrok-free.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  url_api: 'http://localhost:3000/'
+  //url_api: 'http://localhost:3000/'
+  url_api: 'https://0a2a-187-249-108-36.ngrok-free.app'
 };
