3b4fe60467c5f012bc6aeec32e79cf0dec1297ce
Mabel1910 <mabelbaam19@gmail.com>
2024-10-30 01:37:51 -0600

    prueba cypress login-page

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8100',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+
   },
 });
