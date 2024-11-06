a12bfc232c12bb069cc60141f26a0baf4e424d35
ElNao <naopubg28@gmail.com>
2024-11-06 14:26:02 -0600

    finished view product page

--- src/app/products/pages/view-product/view-product.component.ts
+++ src/app/products/pages/view-product/view-product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
-import { Product } from '../../interfaces/Product.interface';
+import { Imagen, Product } from '../../interfaces/Product.interface';
 
 @Component({
   selector: 'app-view-product',
@@ -14,7 +14,22 @@ export class ViewProductComponent implements OnInit{
     private productService:ProductsService,
   ){}
   public id:string = '';
-  public product!:Product;
+  public imgSelected:string = '';
+  public images:Imagen[] = [];
+  public product:Product = {
+    id:              0,
+    nombre_producto: '',
+    precio:          0,
+    descripccion:     '',
+    stock:           0,
+    categoria:       '',
+    rating:          0,
+    descuento:       0,
+    status:          '',
+    tipo:             '',
+    imagen:          [],
+    comentarios:     []
+  };
 
   ngOnInit(): void {
     this.getProductById();
@@ -25,7 +40,16 @@ export class ViewProductComponent implements OnInit{
       this.id = params['product'];
       this.productService.getProductById(this.id).subscribe(res => {
         this.product = res;
+        this.images = res.imagen;
+        this.imgSelected = this.images[0].url_imagen;
       })
+    })
+  }
+  public changeImg(id:number){
+    this.images.forEach(img => {
+      if(img.id === id){
+        this.imgSelected = img.url_imagen;
+      }
     })
   }
 }
