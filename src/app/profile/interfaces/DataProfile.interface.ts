b965da47677adfa7a884abafc280b4f7355d100e
elNao28 <naopubg28@gmail.com>
2024-11-06 17:18:36 -0600

    add interfaces to new endpoints

---
+++ src/app/profile/interfaces/DataProfile.interface.ts
@@ -0,0 +1,26 @@
+export interface RespPersonal{
+  status:number;
+  name:string;
+  lastname:string;
+  motherLastname:string;
+  gender:string;
+  birthdate:string;
+}
+export interface RespCuenta{
+  status:number;
+  email:string;
+  cellphone:string;
+}
+export interface RespSeguridad{
+  status:number;
+  question:number;
+  answer:string;
+}
+export interface RespEnvio{
+  status:number;
+  estado:string;
+  municipio:string;
+  cp:number;
+  colonia:string;
+  referencia:string;
+}
