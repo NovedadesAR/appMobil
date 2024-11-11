52ac090fd9f75fd5a7fe189dfe19b7b8f4c4a681
ElNao <naopubg28@gmail.com>
2024-11-11 12:15:42 -0600

    se termino la pagina de carrito

--- src/app/profile/pages/shopping-car/shopping-car.component.ts
+++ src/app/profile/pages/shopping-car/shopping-car.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+import { DetallesCarrito } from '../../interfaces/RespProductsCard.interface';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-shopping-car',
   templateUrl: './shopping-car.component.html',
   styleUrl: './shopping-car.component.css'
 })
-export class ShoppingCarComponent {
+export class ShoppingCarComponent implements OnInit{
+  constructor(
+    private profileService:ProfileService,
+  ) { }
+  private jwtHelper = new JwtHelperService();
+  private idUser:string = '';
+  public productsCard:DetallesCarrito[] = [];
 
+  ngOnInit(): void {
+    this.getDataCard();
+  }
+
+  public getDataCard(){
+    const token = localStorage.getItem('token');
+    if(token){
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      this.idUser = decodedToken.sub;
+      this.profileService.getProductosCarrito(this.idUser).subscribe(res => {
+        this.productsCard = res.detallesCarrito;
+        console.log(this.productsCard);
+      });
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //url_api: 'https://back-novedadesar-production.up.railway.app/'
-  url_api: 'http://localhost:3000/'
+  url_api: 'https://back-novedadesar-production.up.railway.app/'
+  //url_api: 'http://localhost:3000/'
 };
