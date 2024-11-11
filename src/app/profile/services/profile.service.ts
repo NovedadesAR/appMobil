a853d3c122c264d05e5627565d778d238d8e6c49
ElNao <naopubg28@gmail.com>
2024-11-11 13:37:00 -0600

    se añadio el endpoint para agregar un producto al carrito

--- src/app/profile/services/profile.service.ts
+++ src/app/profile/services/profile.service.ts
@@ -2,11 +2,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Profile } from '../interfaces/Profile.interfac';
-import { RespCuenta, RespEnvio, RespPersonal, RespSeguridad, RespUpdate } from '../interfaces/DataProfile.interface';
-import { UpdatCuenta, UpdatPersonal, UpdatSeguridad, UpdatUbicacion } from '../interfaces/UpdateProfile.interfas';
+import {
+  RespCuenta,
+  RespEnvio,
+  RespPersonal,
+  RespSeguridad,
+  RespUpdate,
+} from '../interfaces/DataProfile.interface';
+import {
+  UpdatCuenta,
+  UpdatPersonal,
+  UpdatSeguridad,
+  UpdatUbicacion,
+} from '../interfaces/UpdateProfile.interfas';
 import { RespCopomex } from '../interfaces/ResponseCopomex.interface';
 import { ResVentas } from '../interfaces/Ventas.interface';
 import { CardResponse } from '../interfaces/RespProductsCard.interface';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { ResponseBack } from 'src/app/login/interfaces/Response-back.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +29,8 @@ export class ProfileService {
 
   private urlApi = environment.url_api;
   private headers = new HttpHeaders({ 'ngrok-skip-browser-warning': '1' });
+  private jwtHerlper = new JwtHelperService();
+
   public checkLogin(): boolean {
     const token = localStorage.getItem('token');
     if (token) {
@@ -29,51 +44,97 @@ export class ProfileService {
     });
   }
   /** Metodos para obtener la informacion de cada seccion */
-  public getDataPersonal(id:string){
-    return this.http.get<RespPersonal>(`${this.urlApi}users/profile/personal/${id}`)
+  public getDataPersonal(id: string) {
+    return this.http.get<RespPersonal>(
+      `${this.urlApi}users/profile/personal/${id}`
+    );
   }
-  public getDataAccount(id:string){
-    return this.http.get<RespCuenta>(`${this.urlApi}users/profile/cuenta/${id}`)
+  public getDataAccount(id: string) {
+    return this.http.get<RespCuenta>(
+      `${this.urlApi}users/profile/cuenta/${id}`
+    );
   }
-  public getDataSegurity(id:string){
-    return this.http.get<RespSeguridad>(`${this.urlApi}users/profile/seguridad/${id}`)
+  public getDataSegurity(id: string) {
+    return this.http.get<RespSeguridad>(
+      `${this.urlApi}users/profile/seguridad/${id}`
+    );
   }
-  public getUbication(id:string){
-    return this.http.get<RespEnvio>(`${this.urlApi}users/profile/ubicacion/${id}`)
+  public getUbication(id: string) {
+    return this.http.get<RespEnvio>(
+      `${this.urlApi}users/profile/ubicacion/${id}`
+    );
   }
   getDataCopomex(cp: string) {
-    return this.http.get<RespCopomex>(`https://api.copomex.com/query/info_cp/${cp}?type=simplified&token=pruebas`);
+    return this.http.get<RespCopomex>(
+      `https://api.copomex.com/query/info_cp/${cp}?type=simplified&token=pruebas`
+    );
   }
   /** Metodos para actualizar las informacion */
-  public updateDataPersonal(id:string,data: UpdatPersonal){
-    return this.http.patch<RespUpdate>(`${this.urlApi}users/profile/update-user/${id}`,data);
+  public updateDataPersonal(id: string, data: UpdatPersonal) {
+    return this.http.patch<RespUpdate>(
+      `${this.urlApi}users/profile/update-user/${id}`,
+      data
+    );
   }
   public updateUserCuenta(id: string, data: UpdatCuenta) {
-    return this.http.patch<RespUpdate>(`${this.urlApi}users/profile/update-user/${id}`, data)
+    return this.http.patch<RespUpdate>(
+      `${this.urlApi}users/profile/update-user/${id}`,
+      data
+    );
   }
   public updateUserSeguridad(id: string, data: UpdatSeguridad) {
-      return this.http.patch<RespUpdate>(`${this.urlApi}users/profile/update-user/${id}`, data)
-    }
+    return this.http.patch<RespUpdate>(
+      `${this.urlApi}users/profile/update-user/${id}`,
+      data
+    );
+  }
   public updateUserPassword(id: string, data: { password: string }) {
-      return this.http.patch<RespUpdate>(`${this.urlApi}users/profile/update-user/${id}`, data)
-    }
+    return this.http.patch<RespUpdate>(
+      `${this.urlApi}users/profile/update-user/${id}`,
+      data
+    );
+  }
   public updateUserUbicacion(id: string, data: UpdatUbicacion) {
-      return this.http.patch<RespUpdate>(`${this.urlApi}users/ubicacion/${id}`, data)
+    return this.http.patch<RespUpdate>(
+      `${this.urlApi}users/ubicacion/${id}`,
+      data
+    );
+  }
+  /** EndPoint para traer las compras **/
+  public getComprasById(id: string) {
+    return this.http.get<ResVentas>(`${this.urlApi}ventas/${id}`);
+  }
+  /** EndPoint para traer los productos del carrito **/
+  public getProductosCarrito(id: string) {
+    return this.http.post<CardResponse>(`${this.urlApi}carrito/get_card`, {
+      id,
+    });
+  }
+  /** EndPoint para eliminar un producto del carrito **/
+  public deleteProductoCarrito(id: string, idProducto: string) {
+    return this.http.delete(`${this.urlApi}carrito/${id}/${idProducto}`);
+  }
+  /** EndPoint para agregar un producto al carrito **/
+  public addProductToCard(data: {
+    cantidad: number;
+    idProduct: number;
+    idUser: string;
+  }) {
+    return this.http.post<ResponseBack>(`${this.urlApi}carrito`, data);
+  }
+
+  addProductToCardSer(id: string) {
+    const idUser = localStorage.getItem('token');
+    if (idUser !== null) {
+      const token = this.jwtHerlper.decodeToken(idUser);
+      const dataCard = {
+        cantidad: 1,
+        idProduct: parseInt(id),
+        idUser: token.sub,
+      };
+      this.addProductToCard(dataCard).subscribe((data) => {
+        console.log(data);
+      });
     }
-    /** EndPoint para traer las compras **/
-    public getComprasById(id: string){
-      return this.http.get<ResVentas>(`${this.urlApi}ventas/${id}`)
-    }
-    /** EndPoint para traer los productos del carrito **/
-    public getProductosCarrito(id: string){
-      return this.http.post<CardResponse>(`${this.urlApi}carrito/get_card`,{id})
-    }
-    /** EndPoint para eliminar un producto del carrito **/
-    public deleteProductoCarrito(id: string, idProducto: string){
-      return this.http.delete(`${this.urlApi}carrito/${id}/${idProducto}`)
-    }
-    /** EndPoint para agregar un producto al carrito **/
-    public addProductoCarrito(id: string, data: any){
-      return this.http.post(`${this.urlApi}carrito/${id}`, data)
-    }
+  }
 }
