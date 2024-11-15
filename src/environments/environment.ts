73e854ba91d50f63f3387c2aa43a0c906ef9b82e
ElNao <naopubg28@gmail.com>
2024-11-15 09:13:20 -0600

    se agrego la funcion viewWillEnter para la recarga de datos de las pantallas del tab

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   //url_api: 'https://back-novedadesar-production.up.railway.app/'
-  //url_api: 'http://localhost:3000/'
-  url_api: 'https://0816-187-249-108-35.ngrok-free.app/'
+  url_api: 'https://2d73-187-249-108-36.ngrok-free.app/'
 };
