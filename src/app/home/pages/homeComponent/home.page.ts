0b57511bdd50cddfc76017e0ddd5c4129b1bdf73
ElNao <naopubg28@gmail.com>
2024-11-19 20:08:47 -0600

    se termino la funcion de feedback

--- src/app/home/pages/homeComponent/home.page.ts
+++ src/app/home/pages/homeComponent/home.page.ts
@@ -7,9 +7,13 @@ import {
 } from '@angular/core';
 import { HomeService } from '../../services/home.service';
 import { ProductoIni } from '../../interfaces/Data-home.interface';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ProductsService, Route } from 'src/app/products/services/products.service';
+import {
+  ProductsService,
+  Route,
+} from 'src/app/products/services/products.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-home',
@@ -19,9 +23,9 @@ import { ProductsService, Route } from 'src/app/products/services/products.servi
 export class HomePage implements AfterViewInit, OnInit {
   constructor(
     private homeService: HomeService,
-    private fb:FormBuilder,
-    private router:Router,
-    private productService:ProductsService
+    private fb: FormBuilder,
+    private router: Router,
+    private productService: ProductsService
   ) {}
 
   @ViewChild('slider') slider!: ElementRef;
@@ -31,14 +35,22 @@ export class HomePage implements AfterViewInit, OnInit {
   public dama: ProductoIni[] = [];
   public caballero: ProductoIni[] = [];
   public isLoader: boolean = true;
+  public isSend: boolean = false;
+  public visible: boolean = false;
+  private idUser: string = '';
+  private jwt = new JwtHelperService();
+  public faildConection: boolean = false;
 
-
-  public faildConection:boolean = false;
-
-  public searchForm = this.fb.group({
-    search: ['',[Validators.required]],
+  public searchForm: FormGroup = this.fb.group({
+    search: ['', [Validators.required]],
   });
 
+  public formCal: FormGroup = this.fb.group({
+    expCompra: [1, [Validators.required]],
+    detalles: [1, [Validators.required]],
+    satOptimizacion: [1, [Validators.required]],
+    fecha: [],
+  });
 
   selectImage(element: ElementRef) {
     const elementDiv = element.nativeElement as HTMLInputElement;
@@ -54,18 +66,22 @@ export class HomePage implements AfterViewInit, OnInit {
     this.selectImage(this.slider);
   }
   ngOnInit(): void {
-    this.homeService.getProductByHome().subscribe((res) => {
-      this.novedades = res.novedades;
-      this.descuentos = res.descuento;
-      this.dama = res.dama;
-      this.caballero = res.caballero;
-      this.isLoader = false;
-      this.faildConection = false;
-      // this.saveRoute();
-    },(Error)=>{
-      console.log("Hay error")
-      this.faildConection = true;
-    });
+    this.checkRating();
+    this.homeService.getProductByHome().subscribe(
+      (res) => {
+        this.novedades = res.novedades;
+        this.descuentos = res.descuento;
+        this.dama = res.dama;
+        this.caballero = res.caballero;
+        this.isLoader = false;
+        this.faildConection = false;
+        // this.saveRoute();
+      },
+      (Error) => {
+        console.log('Hay error');
+        this.faildConection = true;
+      }
+    );
   }
 
   // public saveRoute(){
@@ -77,14 +93,50 @@ export class HomePage implements AfterViewInit, OnInit {
   //   this.productService.routerSet = params;
   // }
 
-  public reloadPage(){
+  public checkRating() {
+    let token = localStorage.getItem('token');
+    if (token !== null) {
+      let tokenN = this.jwt.decodeToken(token);
+      this.idUser = tokenN.sub;
+      this.homeService.checkCompras(tokenN.sub).subscribe((resp) => {
+        if (resp.status === 200) {
+          if (resp.isShopping === true) {
+            this.visible = true;
+          }
+        }
+      });
+    }
+  }
+
+  public reloadPage() {
     this.ngOnInit();
   }
-  public serachProduct(){
-    console.log(this.searchForm.controls['search'].value)
-    if(this.searchForm.invalid) return;
+  public serachProduct() {
+    console.log(this.searchForm.controls['search'].value);
+    if (this.searchForm.invalid) return;
     const value = this.searchForm.controls['search'].value;
     this.searchForm.reset();
     this.router.navigate([`/products/name/${value}`]);
+  }
+
+  public rating() {
+    const fecha = new Date();
+    this.formCal.patchValue({
+      fecha,
+    });
+    if (this.formCal.valid) {
+      this.isSend = true;
+      this.homeService
+        .rating(this.formCal.value, this.idUser)
+        .subscribe((resp) => {
+          console.log(resp);
+          if (resp.status === 200) {
+            this.visible = false;
+            this.isSend = false;
+          }
+        });
+    } else {
+      alert('Todos los campos son obligatorios');
+    }
   }
 }
--- src/app/home/services/home.service.ts
+++ src/app/home/services/home.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { DataInicio } from '../interfaces/Data-home.interface';
+import { ResponseBack } from 'src/app/login/interfaces/Response-back.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,11 @@ export class HomeService {
 
   public getProductByHome(){
     return this.http.get<DataInicio>(`${this.urlApi}products/data-inicio`,{headers: new HttpHeaders({'ngrok-skip-browser-warning':'1'})})
+  }
+  public rating(data:any,id:string){
+    return this.http.post<ResponseBack>(`${this.urlApi}rating/create/${id}`, data)
+  }
+  public checkCompras(id:string){
+    return this.http.get<{status:number,isShopping:boolean}>(`${this.urlApi}ventas/check-venta/${id}`)
   }
 }
